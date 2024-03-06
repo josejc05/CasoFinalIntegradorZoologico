4b55ff2a125b5ef15f89474f06ba8cc26b01c185
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-06 11:38:15 +0100

    Terrestre

--- src/main/java/animal/Terrestre.java
+++ src/main/java/animal/Terrestre.java
@@ -1,4 +1,16 @@
+// En el archivo Terrestre.java
 package animal;
 
-public class Terrestre {
+public class Terrestre extends Animal {
+    // Atributos específicos para un animal terrestre
+
+    @Override
+    public void eat() {
+        // Implementar la lógica para que el animal terrestre coma
+    }
+
+    @Override
+    public void checkHealthStatus() {
+        // Implementar la lógica para verificar el estado de salud del animal terrestre
+    }
 }
