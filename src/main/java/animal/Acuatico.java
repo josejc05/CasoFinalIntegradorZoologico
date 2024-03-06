08abab2e53491fc6843c819360b901fa56b835bf
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-06 11:37:10 +0100

    Acuatico

--- src/main/java/animal/Acuatico.java
+++ src/main/java/animal/Acuatico.java
@@ -1,4 +1,16 @@
+// En el archivo Acuatico.java
 package animal;
 
-public class Acuatico {
+public class Acuatico extends Animal {
+    // Atributos específicos para un animal acuático
+
+    @Override
+    public void eat() {
+        // Implementar la lógica para que el animal acuático coma
+    }
+
+    @Override
+    public void checkHealthStatus() {
+        // Implementar la lógica para verificar el estado de salud del animal acuático
+    }
 }
