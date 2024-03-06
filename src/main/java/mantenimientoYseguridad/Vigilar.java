a8c036cd34712fa2de59535e37e51baf72be4227
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-06 11:21:02 +0100

    Vigilar

--- src/main/java/mantenimientoYseguridad/Vigilar.java
+++ src/main/java/mantenimientoYseguridad/Vigilar.java
@@ -1,4 +1,8 @@
+// En el archivo Vigilar.java
 package mantenimientoYseguridad;
 
 public class Vigilar {
+    public void monitorAccess() {
+        // Implementar la lógica para monitorear accesos y movimientos dentro del zoológico
+    }
 }
