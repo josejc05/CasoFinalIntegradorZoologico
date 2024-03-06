b8a4912a62b8e62f8cb5acb35b471f3fc9a26cff
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-06 11:23:06 +0100

    Recursos

--- src/main/java/mantenimientoYseguridad/Recursos.java
+++ src/main/java/mantenimientoYseguridad/Recursos.java
@@ -1,4 +1,9 @@
+// En el archivo Recursos.java
 package mantenimientoYseguridad;
 
 public class Recursos {
+    private String name;
+    private int quantity;
+
+    // Métodos getter y setter para cada atributo
 }
