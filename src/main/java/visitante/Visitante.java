5ed4dae0de6e227865d08ae0012305a630e76944
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-06 11:25:18 +0100

    Visitante

--- src/main/java/visitante/Visitante.java
+++ src/main/java/visitante/Visitante.java
@@ -1,4 +1,13 @@
+// En el archivo Visitor.java
 package visitante;
 
 public class Visitante {
+    private String name;
+    private String interest;
+
+    // Métodos getter y setter para cada atributo
+
+    public void takeTour() {
+        // Implementar la lógica para que el visitante tome un tour
+    }
 }
