a62f85ede39ef1cefd365c03905008884db4617a
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-06 11:37:46 +0100

    Aviario

--- src/main/java/animal/Aviario.java
+++ src/main/java/animal/Aviario.java
@@ -1,4 +1,16 @@
+// En el archivo Aviario.java
 package animal;
 
-public class Aviario {
+public class Aviario extends Animal {
+    // Atributos específicos para un animal de aviario
+
+    @Override
+    public void eat() {
+        // Implementar la lógica para que el animal de aviario coma
+    }
+
+    @Override
+    public void checkHealthStatus() {
+        // Implementar la lógica para verificar el estado de salud del animal de aviario
+    }
 }
