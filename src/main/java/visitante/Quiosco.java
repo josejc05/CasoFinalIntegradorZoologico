f2cd6f82f8fb3ee0fa58a4bb99eac1b96ed6ef78
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-06 11:24:12 +0100

    Quiosco

--- src/main/java/visitante/Quiosco.java
+++ src/main/java/visitante/Quiosco.java
@@ -1,4 +1,8 @@
+// En el archivo Quiosco.java
 package visitante;
 
 public class Quiosco {
+    public void provideInformation() {
+        // Implementar la lógica para proporcionar información sobre los animales y hábitats
+    }
 }
