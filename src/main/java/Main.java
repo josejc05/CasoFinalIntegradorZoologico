938f3914a97d45e3bbd01c9c9c140e517714dbce
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-06 10:57:17 +0100

    Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import habitat.*;
+import animal.*;
+import visitor.*;
+import resource.*;
+import maintenance.*;
+import security.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Crear instancias de las clases de los diferentes módulos
+        AquaticHabitat aquaticHabitat = new AquaticHabitat();
+        Lion lion = new Lion();
+        Visitor visitor = new Visitor();
+        Food food = new Food();
+        Maintenance maintenance = new Maintenance();
+        Camera camera = new Camera();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Llamar a los métodos de las instancias para ejecutar las funcionalidades del sistema
+        aquaticHabitat.monitorConditions();
+        lion.eat(food);
+        visitor.takeTour();
+        maintenance.performMaintenance();
+        camera.monitorArea();
     }
 }
