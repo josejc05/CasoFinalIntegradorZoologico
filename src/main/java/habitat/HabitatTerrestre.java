b14943e635d439177eb574b28d7416a9680a870d
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-06 11:33:35 +0100

    Habitat Terrestre

--- src/main/java/habitat/HabitatTerrestre.java
+++ src/main/java/habitat/HabitatTerrestre.java
@@ -1,4 +1,13 @@
+// En el archivo TerrestrialHabitat.java
 package habitat;
 
-public class HabitatTerrestre {
+public class HabitatTerrestre extends Habitat {
+    private String vegetationType;
+
+    // Métodos getter y setter para vegetationType
+
+    @Override
+    public void monitorConditions() {
+        // Implementar la lógica para monitorear las condiciones del hábitat terrestre
+    }
 }
