a2acdbd82e221ecd85479ac437c46b8905de811c
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-06 11:21:37 +0100

    Seguimiento

--- src/main/java/mantenimientoYseguridad/Seguimiento.java
+++ src/main/java/mantenimientoYseguridad/Seguimiento.java
@@ -1,4 +1,8 @@
+// En el archivo Seguimiento.java
 package mantenimientoYseguridad;
 
 public class Seguimiento {
+    public void trackMaintenance() {
+        // Implementar la lógica para rastrear el mantenimiento regular y las reparaciones urgentes de las instalaciones
+    }
 }
