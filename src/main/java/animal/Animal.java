6a92b2df58823f6fa534d4069377f1a1817e058e
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-06 11:25:49 +0100

    Animal

--- src/main/java/animal/Animal.java
+++ src/main/java/animal/Animal.java
@@ -1,4 +1,13 @@
+// En el archivo Animal.java
 package animal;
 
-public class Animal {
+public abstract class Animal {
+    private String species;
+    private String diet;
+    private double healthStatus;
+
+    // Métodos getter y setter para cada atributo
+
+    public abstract void eat();
+    public abstract void checkHealthStatus();
 }
