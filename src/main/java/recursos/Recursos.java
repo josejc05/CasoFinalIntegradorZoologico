df95e0a0cc83b084cbcc4e3903e5d6f28fe5e357
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-11 18:07:32 +0100

    .

---
+++ src/main/java/recursos/Recursos.java
@@ -0,0 +1,19 @@
+package recursos;
+
+public class Recursos {
+    private int cantidadDeAlimento;
+    private int cantidadDeMedicinas;
+    private int cantidadDePersonal;
+
+    public Recursos(int cantidadDeAlimento, int cantidadDeMedicinas, int cantidadDePersonal) {
+        this.cantidadDeAlimento = cantidadDeAlimento;
+        this.cantidadDeMedicinas = cantidadDeMedicinas;
+        this.cantidadDePersonal = cantidadDePersonal;
+    }
+
+    // getters y setters
+
+    public void gestionarRecursos() {
+        // Implementación del método gestionarRecursos
+    }
+}
