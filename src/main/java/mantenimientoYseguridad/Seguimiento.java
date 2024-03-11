2a11f79daba2302e1574d8e3e6715124bf91df4a
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-11 18:06:24 +0100

    .

---
+++ src/main/java/mantenimientoYseguridad/Seguimiento.java
@@ -0,0 +1,15 @@
+package mantenimientoYseguridad;
+
+import animal.Animal;
+
+public class Seguimiento {
+    private Animal animal;
+
+    public Seguimiento(Animal animal) {
+        this.animal = animal;
+    }
+
+    public void rastrearComportamiento() {
+        // Implementación del método rastrearComportamiento
+    }
+}
---
+++ src/main/java/mantenimientoYseguridad/Vigilar.java
@@ -0,0 +1,15 @@
+package mantenimientoYseguridad;
+
+import animal.Animal;
+
+public class Vigilar {
+    private Animal animal;
+
+    public Vigilar(Animal animal) {
+        this.animal = animal;
+    }
+
+    public void supervisarAnimal() {
+        // Implementación del método supervisarAnimal
+    }
+}
