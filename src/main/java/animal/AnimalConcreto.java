6dc07eb035ede800e886c278ad030ee993f2adf7
Jose Jurado Cortés <jjuracor@myuax.com>
2024-03-12 12:28:50 +0100

    .

--- src/main/java/animal/AnimalConcreto.java
+++ src/main/java/animal/AnimalConcreto.java
@@ -1,7 +1,12 @@
 package animal;
 
 public class AnimalConcreto extends Animal {
-    public AnimalConcreto(String nombre, String tipoAlimentacion) {
-        super(nombre, tipoAlimentacion);
+    public AnimalConcreto(String nombre, String tipoAlimentacion, String argumento3, String argumento4) {
+        super(nombre, tipoAlimentacion, argumento3, argumento4);
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        // Implementa este método según tus necesidades
     }
 }
